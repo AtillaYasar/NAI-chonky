b24a01f6f920fecd3ea71d6ab1bc3af2f98c823d
Atilla Yasar <112716905+AtillaYasar@users.noreply.github.com>
2023-01-25 07:00:29 +0100

    Add files via upload

--- playground v2.py
+++ playground v2.py
@@ -5,7 +5,21 @@ General overview.
 
     classes.
         - Meta and Story pairs:
-            + 
+            + contain references to one another
+            + Story creates a Meta instance when initialized.
+        - Context
+            + will implement the logic for filling up a context of 2048 tokens
+            + contains references to Meta and Story instances
+        - TrainingData
+            + (intended as something to) provide an overview of:
+                - 2048 chunks
+                - locations of story text and metadata text
+
+    rest.
+        - tokenize/untokenize
+            + placeholder
+        - get_meta
+            + placeholder
 '''
 
 def text_read(fileName):
@@ -30,7 +44,7 @@ def get_meta(string):
     words = string.split(' ')
     word_count = {word:words.count(word) for word in list(set(words))}
     top_n = 5
-    word_count = dict(sorted(word_count.items(), key=lambda tup: tup[1], reverse=True)[:top_n])
+    word_count = dict(sorted(word_count.items(), key=lambda tup: tup[1], reverse=False)[:])
 
     kv_joiner = ':'
     entry_joiner = ', '
@@ -39,6 +53,7 @@ def get_meta(string):
     result = edges[0] + middle + edges[1]
     return result
 
+# a lot of nonsense is here.. need to remove and rewrite stuff.
 class Context:
     def __init__(self):
         self.string = ''
@@ -82,17 +97,21 @@ class Context:
         pass
 
 class Meta:
-    def __init__(self, string, meta_obj):
+    def __init__(self, string, story_object):
         self.string = string
-        self.story_obj = story_obj
+        self.story_object = story_object
 
 class Story:
-    def __init__(self, string, meta_obj):
+    def __init__(self, string):
         self.string = string
-        self.meta_obj = meta_obj
+        self.meta_object = Meta(
+            string=get_meta(self.string),
+            story_object=self
+        )
 
 '''
-Singleton.  (this is not enforced programmatically)
+Singleton class.  (which is not enforced programmatically)
+
 Has a representation of the entire training set:
 - knows how each story's metadata and text content is distributed
 - knows how everything is tokenized
@@ -100,11 +119,14 @@ Has a representation of the entire training set:
     + which stories and metadata are there
     + how many tokens everything takes up
 
-(this allows for elegant visualizations and overviews, hopefully)
+(this potentially allows for elegant visualizations and overviews)
 '''
 class TrainingData:
     def __init__(self):
-        pass
+        self.chunks = {}
+        self.story_objects = {}
+        self.meta_objects = {}
+training_data = TrainingData()
 
 folder = 'stories'
 paths = [f'{folder}/{f}' for f in os.listdir(folder)]
@@ -112,8 +134,19 @@ story_strings = list(map(text_read, paths))
 meta_objects = []
 story_objects = []
 for s in story_strings:
-    
+    s_o = Story(s)
+    m_o = s_o.meta_object
+    story_objects.append(s_o)
+    meta_objects.append(m_o)
 
+for s, m in zip(story_objects, meta_objects):
+    print(s, m)
+    print(s.meta_object == m)
+    print(s == m.story_object)
+    print(m.string)
+    print('---')
+    print(s.string)
+    print('------------------')
 
 
 
